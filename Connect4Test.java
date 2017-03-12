3efa85f75ac84d24d604d1f0a3ae0816e0d37254
stephaniechow <stephaniechow36@gmail.com>
2017-03-12 15:59:28 -0400

    Update Connect4Test.java

--- Connect4Test.java
+++ Connect4Test.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Connect4Test{
-
-public static void main(String[] args){
-}
-
+  public static void main(String[] args){
+    Scanner kb = new Scanner(System.in);
+    System.out.println("Welcome to Connect4!\n In this game, you must get four in a row to win\nGood Luck!");
+    Connect4 board = new Connect4();
+    while(true){
+      board.printArray();
+      System.out.println("What column would you like your chip in?(0-5)");
+      int need = kb.nextInt();
+      while (need>5){
+        System.out.println("That is an invalid column, or the column has already been filled. Please pick another");
+        need = kb.nextInt();
+      }
+      board.addChip(need, 1)
+      
+    }
+  }
 }
