89fedc7ad14fbca61407831de08682410fa0b4ab
stephaniechow <stephaniechow36@gmail.com>
2017-03-12 16:30:05 -0400

    Update Connect4.java

--- Connect4.java
+++ Connect4.java
@@ -1,15 +1,28 @@
 public class Connect4{
-  private String[][] board;
+  private int[][] board;
   public Connect4(){
     board = new int[6][7];
   }
-  public void addchip(int r, int c , int player){
-    if(player==1){
-      board[r][c] = "X";
+  public int findRow(int c){  // Finds the row of the chip for a certain column
+    for(int i=0; i<board.length;i++){
+      if(board[i][c] == 0){
+        return i;
+      }
     }
-    if(player==0){  //Computer
-      board[r][c] = "O";
+    return 10;
+  }
+  public boolean addchip(int c , int player){
+    r = findRow(c);
+    if(r==10){
+      return false;
     }
+    else if(player==1){
+      board[r][c] = 1;
+    }
+    else if(player==2){  //Computer
+      board[r][c] = 2;
+    }
+    return true;
   }
   
   
@@ -17,7 +30,15 @@ public class Connect4{
     for(int k=0; k< board.length; k++){
       System.out.println("");
       for(int j=0; j< board.length[1]; j++){
-        System.out.print(" " + board[k][j]+ " ");
+        if(board[k][j]==1){
+        System.out.print(" X ");
+        }
+        if(board[k][j]==2){
+        System.out.print(" O ");
+        }
+        if(board[k][j]==0){
+        System.out.print("   ");
+        }
       }
     }
   }
