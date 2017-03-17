a1160ea053e6779c420385c8d2ef6640be275090
stephaniechow <stephaniechow36@gmail.com>
2017-03-16 23:01:27 -0400

    Update Connect4Test.java

--- Connect4Test.java
+++ Connect4Test.java
@@ -12,21 +12,21 @@ public class Connect4Test{
       while (need>5){
         System.out.println("That is an invalid column, or the column has already been filled. Please pick another");
         need = kb.nextInt();
-        int lastChipRow = board.addChip(need, 1); //ADDED
-        if(board.addChip(need, 1) == -1){
-          System.out.println("This is not a valid spot");
-        }
-        else{
-          board.moveCPU();
-        }
-        if(board.isFinished(1, lastChipRow, need) == true){ //if the player wins
-          System.out.println("Yay! You won!");
-          break; 
-        }
-        if(board.isFinished(2) == true){  //if the CPU wins //NEED TO STILL IMPLEMENT THIS AFTER AI IS COMPLETE FOR ISFINISHED
-          System.out.println("Oh no, the computer won!");
-          break;
-        }
+      }
+      int lastChipRow = board.addChip(need, 1); //ADDED
+      if(board.addChip(need, 1) == -1){
+        System.out.println("This is not a valid spot");
+      }
+      else{
+        board.moveCPU();
+      }
+      if(board.isFinished(1, lastChipRow, need) == true){ //if the player wins
+        System.out.println("Yay! You won!");
+        break; 
+      }
+      if(board.isFinished(2) == true){  //if the CPU wins //NEED TO STILL IMPLEMENT THIS AFTER AI IS COMPLETE FOR ISFINISHED
+        System.out.println("Oh no, the computer won!");
+        break;
       }
     }
   }
