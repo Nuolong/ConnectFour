34ca5110b04f4a8b07aa171c67fdf982df46e363
Nolan McShea <mcshean@bxscience.edu>
2017-03-16 22:50:25 -0400

    Update Connect4Test.java
    made changes by
    adding line 15
    editing 16 from == false to == -1
    line 22 to add two parameters
    line 26 - after AI method is complete, isFinished can be implemented

--- Connect4Test.java
+++ Connect4Test.java
@@ -12,17 +12,18 @@ public class Connect4Test{
       while (need>5){
         System.out.println("That is an invalid column, or the column has already been filled. Please pick another");
         need = kb.nextInt();
-        if(board.addChip(need, 1)== false){
+        int lastChipRow = board.addChip(need, 1); //ADDED
+        if(board.addChip(need, 1) == -1){
           System.out.println("This is not a valid spot");
         }
         else{
           board.moveCPU();
         }
-        if(board.isFinished(1) == true){ //if the player wins
+        if(board.isFinished(1, lastChipRow, need) == true){ //if the player wins
           System.out.println("Yay! You won!");
           break; 
         }
-        if(board.isFinished(2) == true){  //if the CPU wins
+        if(board.isFinished(2) == true){  //if the CPU wins //NEED TO STILL IMPLEMENT THIS AFTER AI IS COMPLETE FOR ISFINISHED
           System.out.println("Oh no, the computer won!");
           break;
         }
